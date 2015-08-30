5234b98cfef1996214afd715cd89a7a0ab590a06
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-08-30 02:32:47 +0200

    [dTRAM] added API to __init__.py

--- rewcore/__init__.py
+++ rewcore/__init__.py
@@ -1,4 +1,4 @@
 
 from .lse import logsumexp, logsumexp_pair
 from .wham import wham_fi, wham_fk, wham_normalize
-
+from .dtram import log_nu_K_i_setter, log_nu_K_i_equation, f_i_equation, p_K_ij_equation, f_K_equation
