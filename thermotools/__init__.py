8826153554e8472d8c3b99cb1df0f78d916490e7
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-09-02 21:53:18 +0200

    amending package init

--- thermotools/__init__.py
+++ thermotools/__init__.py
@@ -1,4 +1,11 @@
+r"""
+thermotools is a lowlevel implementation toolbox for the analyis of free energy calculations
+"""
 
 from .lse import logsumexp, logsumexp_pair
 from .wham import wham_fi, wham_fk, wham_normalize
 from .dtram import dtram_set_lognu, dtram_lognu, dtram_fi, dtram_pk, dtram_p, dtram_fk
+
+__author__ = "Christoph Wehmeyer"
+__copyright__ = "Copyright 2015 Computational Molecular Biology Group, FU-Berlin"
+__email__ = "christoph.wehmeyer AT fu-berlin DOT de"
