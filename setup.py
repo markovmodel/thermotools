3ed7bf5add32f2af116f60f7fbfc3589eec444f6
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-09-02 22:21:35 +0200

    switch to LGPLv3+

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+
+# This file is part of thermotools.
+#
+# Copyright 2015 Computational Molecular Biology Group, Freie Universitaet Berlin (GER)
+#
+# thermotools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 from setuptools import setup
 from distutils.core import Extension
 from sys import exit as sys_exit
@@ -50,7 +71,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Natural Language :: English',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX :: Linux',
@@ -69,7 +90,7 @@ setup(
     url='',
     author='Christoph Wehmeyer',
     author_email='christoph.wehmeyer@fu-berlin.de',
-    license='Simplified BSD License',
+    license='LGPLv3+',
     setup_requires=[
         'numpy>=1.7.1',
         'cython>=0.15',
