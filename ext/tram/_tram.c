markovmodel/thermotools

commit 755900d16ee01866f47bcf9ca3af965b7ff563c3

[TRAM] changed initialization of Lagrange multipliers to be compatible with allowed T_ij range

ext/tram/_tram.c
@@ -41,7 +41,8 @@ void _init_lagrangian_mult(int *count_matrices, int n_therm_states, int n_conf_s
         {
             sum = 0;
             for(j=0; j<n_conf_states; ++j)
-                sum += count_matrices[KMM + i * n_conf_states + j];
+                sum += 0.5 * (count_matrices[KMM + i * n_conf_states + j]+
+                              count_matrices[KMM + j * n_conf_states + i]);
             log_lagrangian_mult[K * n_conf_states + i] = log(THERMOTOOLS_TRAM_PRIOR + sum);
         }
     }

commit 65acd05d0692889cf041258b43e30aae603faaf5

[TRAM] fixed bug that was introduced by my commit 755900d16ee01866f47bcf9ca3af965b7ff563c3

ext/tram/_tram.c
@@ -33,13 +33,13 @@ void _init_lagrangian_mult(int *count_matrices, int n_therm_states, int n_conf_s
 {
     int i, j, K;
     int MM = n_conf_states * n_conf_states, KMM;
-    int sum;
+    double sum;
     for(K=0; K<n_therm_states; ++K)
     {
         KMM = K * MM;
         for(i=0; i<n_conf_states; ++i)
         {
-            sum = 0;
+            sum = 0.0;
             for(j=0; j<n_conf_states; ++j)
                 sum += 0.5 * (count_matrices[KMM + i * n_conf_states + j]+
                               count_matrices[KMM + j * n_conf_states + i]);