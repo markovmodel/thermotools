59d46b0b09fd81f7e4896bd738d66277cbb9b21f
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-10-30 13:47:53 +0100

    [DTRAM] finishing update_lagrangian_mult_mk2() with tests

--- test/test_dtram.py
+++ test/test_dtram.py
@@ -30,12 +30,11 @@ def test_lognu_zero_counts_mk2():
     log_lagrangian_mult = np.zeros(shape=(nt, nm), dtype=np.float64)
     bias_energies = np.zeros(shape=(nt, nm), dtype=np.float64)
     conf_energies = np.zeros(shape=(nm,), dtype=np.float64)
-    C_K_ij = np.zeros(shape=(nt, nm, nm), dtype=np.intc) # C_K_ii = 1.0E-10 (internal prior)
+    C_K_ij = np.zeros(shape=(nt, nm, nm), dtype=np.intc)
     scratch_i = np.zeros(shape=(nm,), dtype=np.float64)
     new_log_lagrangian_mult = np.zeros(shape=(nt, nm), dtype=np.float64)
-    ref_log_lagrangian_mult = np.log(1.0E-10*np.ones(shape=(nt, nm), dtype=np.float64)) # (prior)
     update_lagrangian_mult_mk2(log_lagrangian_mult, bias_energies, conf_energies, C_K_ij, scratch_i, new_log_lagrangian_mult)
-    assert_allclose(new_log_lagrangian_mult, ref_log_lagrangian_mult, atol=1.0E-16)
+    assert_allclose(new_log_lagrangian_mult, -np.inf, atol=1.0E-16)
 
 def test_lognu_all_factors_unity_mk2():
     nm = 200
@@ -46,9 +45,8 @@ def test_lognu_all_factors_unity_mk2():
     C_K_ij = np.ones(shape=(nt, nm, nm), dtype=np.intc)
     scratch_i = np.zeros(shape=(nm,), dtype=np.float64)
     new_log_lagrangian_mult = np.zeros(shape=(nt, nm), dtype=np.float64)
-    ref_log_lagrangian_mult = np.log(nm*np.ones(shape=(nt, nm), dtype=np.float64))
     update_lagrangian_mult_mk2(log_lagrangian_mult, bias_energies, conf_energies, C_K_ij, scratch_i, new_log_lagrangian_mult)
-    assert_allclose(new_log_lagrangian_mult, ref_log_lagrangian_mult, atol=1.0E-16)
+    assert_allclose(new_log_lagrangian_mult, np.log(nm), atol=1.0E-16)
 
 def test_lognu_K_range_mk2():
     nm = 200
@@ -61,9 +59,8 @@ def test_lognu_K_range_mk2():
     C_K_ij = np.ones(shape=(nt, nm, nm), dtype=np.intc)
     scratch_i = np.zeros(shape=(nm,), dtype=np.float64)
     new_log_lagrangian_mult = np.zeros(shape=(nt, nm), dtype=np.float64)
-    ref_log_lagrangian_mult = np.log(nm*np.ones(shape=(nt, nm), dtype=np.float64))
     update_lagrangian_mult_mk2(log_lagrangian_mult, bias_energies, conf_energies, C_K_ij, scratch_i, new_log_lagrangian_mult)
-    assert_allclose(new_log_lagrangian_mult, ref_log_lagrangian_mult, atol=1.0E-16)
+    assert_allclose(new_log_lagrangian_mult, np.log(nm), atol=1.0E-16)
 
 
 def test_fi_zero_counts_mk2():
