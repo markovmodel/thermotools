2355df261fd9ddf6b31c29746fe5f058918e9cbd
jouno <fab@physik.tu-berlin.de>
2015-11-20 21:24:21 +0100

    [TRAMdirect] adapted direct space TRAM to new signature of estimate()

--- test/test_random_model.py
+++ test/test_random_model.py
@@ -48,46 +48,63 @@ def draw_transition_counts(transition_matrices, n_samples, x0):
     return count_matrices, conf_state_sequence, state_counts
 
 class TestRandom(unittest.TestCase):
-    def test_tram(self):
-        
+    @classmethod
+    def setUpClass(cls):
         n_therm_states = 4
         n_conf_states = 4
         n_samples = 10000
 
-        bias_energies = np.zeros(shape=(n_therm_states, n_conf_states), dtype=np.float64)
-        T = np.zeros(shape=(n_therm_states, n_conf_states, n_conf_states), dtype=np.float64)
+        cls.bias_energies = np.zeros(shape=(n_therm_states, n_conf_states), dtype=np.float64)
+        cls.T = np.zeros(shape=(n_therm_states, n_conf_states, n_conf_states), dtype=np.float64)
         while True:
             # generate two random stionary distributions
             for k in range(n_therm_states):
-                bias_energies[k,:] = -np.log(np.random.rand(n_conf_states))
+                cls.bias_energies[k,:] = -np.log(np.random.rand(n_conf_states))
                 if k>0:
-                    bias_energies[k,:] += np.random.rand()
+                    cls.bias_energies[k,:] += np.random.rand()
 
             # generate transition matrices
             for k in range(n_therm_states):
-                T[k,:,:] = T_matrix(bias_energies[k,:])
+                cls.T[k,:,:] = T_matrix(cls.bias_energies[k,:])
 
-            count_matrices, conf_state_sequence, state_counts = draw_transition_counts(T, n_samples, 0)
+            cls.count_matrices, cls.conf_state_sequence, cls.state_counts = draw_transition_counts(cls.T, n_samples, 0)
 
-            if msmtools.analysis.is_connected(count_matrices.sum(axis=0), directed=True):
+            if msmtools.analysis.is_connected(cls.count_matrices.sum(axis=0), directed=True):
                 break
-                
-        bias_energies_sh = bias_energies - bias_energies[0,:]
-        bias_energies_sh = np.ascontiguousarray(bias_energies_sh[:,conf_state_sequence])
 
+        cls.bias_energies_sh = cls.bias_energies - cls.bias_energies[0,:]
+        cls.bias_energies_sh = np.ascontiguousarray(cls.bias_energies_sh[:,cls.conf_state_sequence])
+
+    def test_tram(self):
         biased_conf_energies, conf_energies, therm_energies, log_lagrangian_mult, error_history, logL_history = tram.estimate(
-            count_matrices, state_counts, bias_energies_sh, conf_state_sequence,
+            self.count_matrices, self.state_counts, self.bias_energies_sh, self.conf_state_sequence,
             maxiter=1000000, maxerr=1.0E-10, lll_out=10)
         transition_matrices = tram.estimate_transition_matrices(
-        log_lagrangian_mult, biased_conf_energies, count_matrices, None)
-        
-        biased_conf_energies -= np.min(biased_conf_energies)
-        bias_energies -=  np.min(bias_energies)
+            log_lagrangian_mult, biased_conf_energies, self.count_matrices, None)
 
-        nz = np.where(state_counts>0)
+        biased_conf_energies -= np.min(biased_conf_energies)
+        bias_energies =  self.bias_energies - np.min(self.bias_energies)
+
+        nz = np.where(self.state_counts>0)
         assert not np.any(np.isinf(log_lagrangian_mult[nz]))
         assert np.allclose(biased_conf_energies, bias_energies, atol=0.1)
-        assert np.allclose(transition_matrices, T, atol=0.1)
+        assert np.allclose(transition_matrices, self.T, atol=0.1)
+        assert np.all(logL_history[-1]+1.E-5>=np.array(logL_history[0:-1]))
+
+    def test_tram_direct(self):
+        biased_conf_energies, conf_energies, therm_energies, log_lagrangian_mult, error_history, logL_history = tram_direct.estimate(
+            self.count_matrices, self.state_counts, self.bias_energies_sh, self.conf_state_sequence,
+            maxiter=1000000, maxerr=1.0E-10, lll_out=10)
+        transition_matrices = tram.estimate_transition_matrices(
+            log_lagrangian_mult, biased_conf_energies, self.count_matrices, None)
+
+        biased_conf_energies -= np.min(biased_conf_energies)
+        bias_energies =  self.bias_energies - np.min(self.bias_energies)
+
+        nz = np.where(self.state_counts>0)
+        assert not np.any(np.isinf(log_lagrangian_mult[nz]))
+        assert np.allclose(biased_conf_energies, bias_energies, atol=0.1)
+        assert np.allclose(transition_matrices, self.T, atol=0.1)
         assert np.all(logL_history[-1]+1.E-5>=np.array(logL_history[0:-1]))
 
 if __name__ == "__main__":
