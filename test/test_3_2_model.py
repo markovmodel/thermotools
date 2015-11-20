ad5578326f6f0451ec4bb07bee32c8f663b33c73
jouno <fab@physik.tu-berlin.de>
2015-11-20 21:19:59 +0100

    [MBARdirect] adapted MABR direct to the new signature of estimate()

--- test/test_3_2_model.py
+++ test/test_3_2_model.py
@@ -19,6 +19,7 @@ import thermotools.wham as wham
 import thermotools.tram as tram
 import thermotools.tram_direct as tram_direct
 import thermotools.mbar as mbar
+import thermotools.mbar_direct as mbar_direct
 import thermotools.dtram as dtram
 import thermotools.tram as tram
 import numpy as np
@@ -120,6 +121,18 @@ class TestThreeTwoModel(object):
         assert_allclose(biased_conf_energies, self.biased_conf_energies, atol=maxerr)
         assert_allclose(conf_energies, self.conf_energies, atol=maxerr)
         assert_allclose(therm_energies, self.therm_energies, atol=maxerr)
+    def test_mbar_direct(self):
+        bias_energy_sequence = np.ascontiguousarray(
+            self.bias_energies[:, self.conf_state_sequence_ind])
+        therm_energies, conf_energies, biased_conf_energies, err_history = mbar_direct.estimate(
+            self.state_counts_ind.sum(axis=1),
+            bias_energy_sequence,
+            self.conf_state_sequence_ind,
+            maxiter=10000, maxerr=1.0E-15)
+        maxerr = 1.0E-1
+        assert_allclose(biased_conf_energies, self.biased_conf_energies, atol=maxerr)
+        assert_allclose(conf_energies, self.conf_energies, atol=maxerr)
+        assert_allclose(therm_energies, self.therm_energies, atol=maxerr)
     def test_dtram(self):
         therm_energies, conf_energies, log_lagrangian_mult, err, lll = dtram.estimate(
             self.count_matrices, self.bias_energies, maxiter=10000, maxerr=1.0E-15)
@@ -144,3 +157,17 @@ class TestThreeTwoModel(object):
         assert_allclose(transition_matrices, self.transition_matrices, atol=maxerr)
         # lower bound on the log-likelihood must be maximal at convergence
         assert np.all(logL_history[-1]+1.E-5>=logL_history[0:-1])
+    def test_tram_direct(self):
+        bias_energies = np.ascontiguousarray(self.bias_energies[:,self.conf_state_sequence])
+        biased_conf_energies, conf_energies, therm_energies, log_lagrangian_mult, error_history, logL_history = tram_direct.estimate(
+            self.count_matrices, self.state_counts, bias_energies, self.conf_state_sequence,
+            maxiter=10000, maxerr=1.0E-12, lll_out=10)
+        transition_matrices = tram.estimate_transition_matrices(
+            log_lagrangian_mult, biased_conf_energies, self.count_matrices, None)
+        maxerr = 1.0E-1
+        assert_allclose(biased_conf_energies, self.biased_conf_energies, atol=maxerr)
+        assert_allclose(conf_energies, self.conf_energies, atol=maxerr)
+        assert_allclose(therm_energies, self.therm_energies, atol=maxerr)
+        assert_allclose(transition_matrices, self.transition_matrices, atol=maxerr)
+        # lower bound on the log-likelihood must be maximal at convergence
+        assert np.all(logL_history[-1]+1.E-5>=logL_history[0:-1])        
