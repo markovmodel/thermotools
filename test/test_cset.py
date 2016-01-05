76bce45a3950a58a606e9fe6c0982aae4592ebf1
jouno <fab@physik.tu-berlin.de>
2016-01-05 20:22:08 +0100

    [cset] added unit test

--- test/test_cset.py
+++ test/test_cset.py
@@ -4,81 +4,72 @@ from six.moves import range
 
 import numpy as np
 import thermotools.cset as cset
-
-def tower_sample(distribution):
-    cdf = np.cumsum(distribution)
-    rnd = np.random.rand() * cdf[-1]
-    return np.searchsorted(cdf, rnd)
-
-def T_matrix(energy):
-    n = energy.shape[0]
-    metropolis = energy[np.newaxis, :] - energy[:, np.newaxis]
-    metropolis[(metropolis < 0.0)] = 0.0
-    selection = np.zeros((n,n))
-    selection += np.diag(np.ones(n-1)*0.5,k=1)
-    selection += np.diag(np.ones(n-1)*0.5,k=-1)
-    selection[0,0] = 0.5
-    selection[-1,-1] = 0.5
-    metr_hast = selection * np.exp(-metropolis)
-    for i in range(metr_hast.shape[0]):
-        metr_hast[i, i] = 0.0
-        metr_hast[i, i] = 1.0 - metr_hast[i, :].sum()
-    return metr_hast
-
-def draw_transition_counts(transition_matrices, n_samples, x0):
-    """generates a discrete Markov chain"""
-    count_matrices = np.zeros(shape=transition_matrices.shape, dtype=np.intc)
-    conf_state_sequence = np.zeros(shape=(transition_matrices.shape[0]*(n_samples+1),), dtype=np.intc)
-    state_counts = np.zeros(shape=transition_matrices.shape[0:2], dtype=np.intc)
-    h = 0
-    for K in range(transition_matrices.shape[0]):
-        x = x0
-        state_counts[K, x] += 1
-        conf_state_sequence[h] = x
-        h += 1
-        for s in range(n_samples):
-            x_new = tower_sample(transition_matrices[K, x, :])
-            count_matrices[K, x, x_new] += 1
-            x = x_new
-            state_counts[K, x] += 1
-            conf_state_sequence[h] = x
-            h += 1
-    return count_matrices, conf_state_sequence, state_counts
+import thermotools.util as util
 
 class TestCset(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        n_therm_states = 4
-        n_conf_states = 4
-        n_samples = 10000
-        bias_energies = np.zeros((n_therm_states,n_conf_states))
-        bias_energies[0,:] = np.array([0.0, 15.0, 15.0, 0.0 ]) 
-        bias_energies[1,:] = np.array([15.0, 0.0, 15.0, 20.0 ])+4  
-        bias_energies[2,:] = np.array([15.0,15.0,  0.0, 20.0 ])+8
-        bias_energies[3,:] = np.array([15.0, 0.0, 15.0, 20.0 ])+12
-        bias_energies_sh = bias_energies - bias_energies[0,:]
+        m1 = 10
+        s1 = 1
+        m2 = 20
+        s2 = 1
+        n_samples = 1000
 
-        T = np.zeros(shape=(n_therm_states, n_conf_states, n_conf_states), dtype=np.float64)
-        tramtrajs = [np.zeros((n_samples, 2+n_therm_states)) for k in range(n_therm_states)]
-        for k in range(n_therm_states):
-            T[k,:,:] = T_matrix(bias_energies[k,:])
-            count_matrices, conf_state_sequence, state_counts = draw_transition_counts(T, n_samples, 0) # ??? retuned data format
-            tramtrajs[k][:,0] = k
-            tramtrajs[k][:,1] = conf_state_sequence
-            tramtrajs[k][:,2:] = bias_energies_sh[:,conf_state_sequence]
+        def u(x):
+            return np.array([0.5*((x-m1)/s1)**2, 0.5*((x-m2)/s2)**2]).T
 
+        x1 = np.random.randn(n_samples)*s1 + m1
+        tramtraj1 = np.zeros((n_samples,4))
+        tramtraj1[:,0]  = 0
+        tramtraj1[:,1]  = x1<20
+        tramtraj1[:,2:] = u(x1)
+
+        tramtraj2 = np.zeros((n_samples,4))
+        x2 = np.random.randn(n_samples)*s2 + m2
+        tramtraj2[:,0]  = 1
+        tramtraj2[:,1]  = x2<20
+        tramtraj2[:,2:] = u(x2)
+        
+        tramtrajs = [tramtraj1, tramtraj2]
+        tram_sequence = np.concatenate(tramtrajs)
+        state_counts = util.state_counts(tramtrajs)
+        count_matrices = util.count_matrices(
+            [np.ascontiguousarray(t[:, :2]).astype(np.intc) for t in tramtrajs], 1,
+            sliding='sliding', sparse_return=False, nstates=2)
+        cls.tram_sequence = tram_sequence
+        cls.state_counts = state_counts
         cls.count_matrices = count_matrices
-        cls.state_counts = cls.state_counts
-        cls.tramtrajs = tramtrajs
-        cls.tram_traj = np.concatenate(self.tram_trajs)
+
+    def test_summed_count_matrix(self):
+        csets, projected_cset = cset.compute_csets_TRAM('summed_count_matrix', self.state_counts, self.count_matrices, self.tram_sequence)
+        np.testing.assert_allclose(csets[0], np.array([1]))
+        np.testing.assert_allclose(csets[1], np.array([0, 1]))
+        np.testing.assert_allclose(projected_cset, np.array([0,1]))
+
+    #def test_strong_in_every_ensemble(self):
+    #    csets, projected_cset = cset.compute_csets_TRAM('strong_in_every_ensemble', self.state_counts, self.count_matrices, self.tram_sequence)
+    #    np.testing.assert_allclose(csets[0], np.array([1]))
+    #    np.testing.assert_allclose(csets[1], np.array([0, 1]))
+    #    np.testing.assert_allclose(projected_cset, np.array([0,1]))
+
+    def test_cset_neighbors(self):
+        csets, projected_cset = cset.compute_csets_TRAM('neighbors', self.state_counts, self.count_matrices, self.tram_sequence, nn=1)
+        np.testing.assert_allclose(csets[0], np.array([1]))
+        np.testing.assert_allclose(csets[1], np.array([0, 1]))
+        np.testing.assert_allclose(projected_cset, np.array([0,1]))
 
     def test_cset_post_hoc_RE(self):
-        csets, projected_cset = cset.compute_csets_TRAM('neighbors', self.state_counts, self.count_matrices, self.tram_traj, nn=1)
-        assert np.equal(projected_cset, np.array([0,1,2]))
+        csets, projected_cset = cset.compute_csets_TRAM('post_hoc_RE', self.state_counts, self.count_matrices, self.tram_sequence)
+        np.testing.assert_allclose(csets[0], np.array([]))
+        np.testing.assert_allclose(csets[1], np.array([0, 1]))
+        np.testing.assert_allclose(projected_cset, np.array([0, 1]))
 
-    def test_cset_post_hoc_RE(self):
-        csets, projected_cset = cset.compute_csets_TRAM('post_hoc_RE', self.state_counts, self.count_matrices, self.tram_traj)
-        assert np.equal(projected_cset, np.array([0,1,2]))
+    def test_cset_BAR_variance(self):
+        csets, projected_cset = cset.compute_csets_TRAM('BAR_variance', self.state_counts, self.count_matrices, self.tram_sequence)
+        np.testing.assert_allclose(csets[0], np.array([]))
+        np.testing.assert_allclose(csets[1], np.array([0, 1]))
+        np.testing.assert_allclose(projected_cset, np.array([0, 1]))
+
 
 if __name__ == "__main__":
     unittest.main()
